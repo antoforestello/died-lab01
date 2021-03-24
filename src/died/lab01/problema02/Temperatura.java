044ba217a24e8a924d6a01040f9bb07b2f2b9f29
antoforestello <anto.forestello@gmail.com>
2021-03-24 09:42:37 -0300

    Resolvi el ejercicio 2

---
+++ src/died/lab01/problema02/Temperatura.java
@@ -0,0 +1,72 @@
+package died.lab01.problema02;
+
+public class Temperatura {
+	private double grados;
+	private Escala escala;
+	
+	public Temperatura() {
+		grados=0;
+		escala= Escala.CELCIUS;
+	}
+	public Temperatura(double grados, Escala escala) {
+		this.grados=grados;
+		this.escala=escala; 
+	}
+	
+	public String toString() {
+		String stringNumero= String.valueOf(this.grados);
+		String stringEscala="";
+		switch(this.escala) {
+		case CELCIUS: stringEscala = " C°";
+					break;
+		case FAHRENHEIT: stringEscala = " F°";
+					break;
+		}
+		String salida= stringNumero.concat(stringEscala);
+		return salida;
+	}
+	
+	public double asCelcius() {
+		double resultado; 
+		if(this.escala == Escala.CELCIUS) {
+			resultado = this.grados;
+		}else {
+			resultado = (this.grados -32) * 5 / 9;
+		}
+		return resultado;
+	}
+	
+	public double asFahrenheit() {
+		double resultado; 
+		if(this.escala == Escala.FAHRENHEIT) {
+			resultado = this.grados;
+		}else {
+			resultado = (this.grados * 9 / 5 )  + 32;
+		}
+		return resultado;
+	}
+	
+	public void aumentar(Temperatura temperatura) {
+		if(temperatura.grados > 0.0) {
+			Escala estaEscala = this.escala;
+			switch(estaEscala) {
+			case CELCIUS:this.grados = this.grados + temperatura.asCelcius();
+			break;
+			case FAHRENHEIT: this.grados = this.grados + temperatura.asFahrenheit();
+			break;
+			}	
+		}
+	}
+	
+	public void disminuir(Temperatura temperatura) {
+		if(temperatura.grados > 0.0) {
+			Escala estaEscala = this.escala;
+			switch(estaEscala) {
+			case CELCIUS:this.grados = this.grados - temperatura.asCelcius();
+			break;
+			case FAHRENHEIT: this.grados = this.grados - temperatura.asFahrenheit();
+			break;
+			}	
+		}
+	}
+}
