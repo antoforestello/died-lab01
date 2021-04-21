dabb9ac0bda78be5d9c3a56a7cc151bae3d505e2
antoforestello <anto.forestello@gmail.com>
2021-04-21 14:15:41 -0300

    trabajo completo

---
+++ src/reparaFix/Contratable.java
@@ -0,0 +1,12 @@
+package reparaFix;
+
+import java.time.LocalDate;
+
+public interface Contratable {
+	//metodos
+	public Double costo();
+	public boolean finalizado();
+	public boolean esAlquiler();
+	public boolean esTrabajo();
+	public boolean tieneTrabajadorAsignado();
+}
